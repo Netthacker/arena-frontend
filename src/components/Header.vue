7ea6bf26c2773147785c14c9790607887ecbfb6e
netthacker <brigido.lbo@gmail.com>
2024-05-27 11:01:47 -0300

    Melhorado header dos componentes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,9 +21,21 @@ export default defineComponent({
 
 
 <template>
-  <v-app-bar app color="primary" dark>
-    <v-toolbar-title>Dashboard</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-btn @click="logout" color="red" dark>Logout</v-btn>
-  </v-app-bar>
+  <v-card>
+    <v-row>
+      <v-col cols="12" md="12">
+        <v-app-bar app color="primary" dark>
+              <v-toolbar-title style="margin-left:15px">Arena Online</v-toolbar-title>
+            <v-col cols="12" md="1">
+              <v-btn
+                color="grey-darken-4"
+                icon="mdi-logout"
+                @click="logout"
+                dark>
+              </v-btn>
+            </v-col>
+        </v-app-bar>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,13 +9,50 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+const lightTheme = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#FFA500',  // Laranja
+    'primary-darken-1': '#FF8C00',
+    secondary: '#FFA500', // Laranja
+    'secondary-darken-1': '#FF8C00',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
+const darkTheme = {
+  dark: true,
+  colors: {
+    background: '#000000',
+    surface: '#000000',
+    primary: '#FFA500',  // Laranja
+    'primary-darken-1': '#FF8C00',
+    secondary: '#FFA500', // Laranja
+    'secondary-darken-1': '#FF8C00',
+    error: '#CF6679',
+    info: '#BB86FC',
+    success: '#03DAC6',
+    warning: '#FB8C00',
+  },
+}
+
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'darkTheme',
+    themes:{
+      lightTheme,darkTheme,
+    },
   },
+
   icons: {
     defaultSet: 'mdi',
     aliases,
